d76e709e280b7a57aaad5a24e0fb9d64c650f17b
unpredictableWolf <xgamer85bartekx@gmail.com>
2022-02-03 17:31:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const path = require('path')
 const StackBlur = require('stackblur-canvas');
 const fs = require('fs')
 
-// example url: http://localhost:8080/?avatarUrl=avatarUrlHere&username=usernameOnImage&tag=tagOnImage
-app.get('/', async function (req, res) {
+// example url: http://localhost:8080/?avatarUrl=avatarUrlHere&username=usernameOnImage&tag=tagOnImage&number=312
+app.get('*', async function (req, res) {
     if(!req.query.username || !req.query.tag || !req.query.avatarUrl) return res.send({ "error": true, "message": "url must contain all GET options (username, tag, avatarUrl)" })
     try {
         const nickname = `${req.query.username}#${req.query.tag}`
@@ -17,8 +17,6 @@ app.get('/', async function (req, res) {
         const canvas = Canvas.createCanvas(properties.width, properties.height);
         const context = canvas.getContext('2d');
 
-        // context.fillStyle = '#3644d6';
-        // context.fillRect(0, 0, properties.width, properties.height);
         const background = await Canvas.loadImage(path.join(__dirname, './assets/welcomeImgBg.jpg'))
         context.drawImage(background, 1332 - 1332 * 1.5, -80, 1322, 850) // 1332 x 850
 
@@ -41,18 +39,18 @@ app.get('/', async function (req, res) {
             context.font = fontSize + "px " + "Roboto-Regular";
         } while (context.measureText(nickname).width > canvas.width)
 
-        // context.font = `${fontSize}px Roboto-Regular`
-        // if(context.measureText(nickname).width > properties.width - 10){
-        //     fontSize = (context.measureText(nickname).width / properties.width) - fontSize + 14;
-        // }
-
         context.fillStyle = '#bababa'
         context.font = `${fontSize}px Roboto-Regular`
         context.fillText(nickname, (properties.width - 10) / 2, properties.height / 1.2 - 19)
         context.fillStyle = '#dadada'
         context.fillText(nickname, (properties.width - 10) / 2, properties.height / 1.2 - 20)
-        // console.log(context.measureText(nickname))
 
+        if(req.query.number){
+            context.fillStyle = '#bababa'
+            context.font = `12px Roboto-Regular`
+            context.fillText(`Dołączyłxś jako osoba nr ${req.query.number}`, (properties.width - 10) / 2, properties.height / 1.2 + 40)
+        }
+        
         context.beginPath()
         context.arc(96 + properties.width / 4 - 10, 96 + 30, 110, 0, Math.PI * 2, true)
         context.arc(40 + properties.width / 4 - 10, 40 + 30, 40, 0, Math.PI * 2, true)
@@ -66,13 +64,15 @@ app.get('/', async function (req, res) {
         const avatar = await Canvas.loadImage(req.query.avatarUrl); // recommended image size is 4096px for the best quality
         context.drawImage(avatar, properties.width / 4 - 10, 30, 192, 192);
 
-        res.send({ "buffer": canvas.toBuffer(), "image": "welcome.png" });
+        res.send({ "buffer": canvas.toBuffer(), "image": "hewwo.png" });
+        // fs.writeFileSync('hewwo.png', canvas.toBuffer())
     } catch (err) {
         res.send({ "error": true })
+        console.log(err)
     }
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(80, function () {
     var host = server.address().address;
     var port = server.address().port;
 
